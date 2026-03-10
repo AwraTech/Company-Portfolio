780dfac53b06b682ca25b939ce373596baede83a
dopi95 <elyasyenealem@gmail.com>
2026-03-10 11:02:03 +0300

    update the folder structure

---
+++ client/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fCRdj+ez8jQtuyfzf0qKa5Lh3uQdPtGcdeuqdQV/qJM=\"\n}"
---
+++ client/dev/static/chunks/src_app_layout_tsx_1cf6b850._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__44bc443a._.css",
+  "static/chunks/node_modules_next_c1d7935c._.js",
+  "static/chunks/node_modules_react-icons_md_index_mjs_65e576c2._.js",
+  "static/chunks/node_modules_react-icons_io5_index_mjs_9bce8f6c._.js",
+  "static/chunks/node_modules_react-icons_lib_844c6c50._.js",
+  "static/chunks/src_b744621a._.js"
+],
+    source: "dynamic"
+});
